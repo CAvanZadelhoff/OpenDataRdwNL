d8ef125d74932d6d2544cd09e163dee231e8b104
Poriya Monfared <poriya.monfared@gmail.com>
2021-02-01 22:37:09 +0330

    fixed a property wrong title

--- OpenDataRdwNL.Models/ApiResponses/CarVehicleBodyWorkDetailApiResponse.cs
+++ OpenDataRdwNL.Models/ApiResponses/CarVehicleBodyWorkDetailApiResponse.cs
@@ -2,7 +2,7 @@
 {
     public class CarVehicleBodyWorkDetailApiResponse
     {
-        public string kentaken { get; set; }
+        public string kenteken { get; set; }
         public int carrosserie_volgnummer { get; set; }
         public string carrosserietype { get; set; }
         public string type_carrosserie_europese_omschrijving { get; set; }
